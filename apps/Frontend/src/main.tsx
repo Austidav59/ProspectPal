4a9466f569c85e17ab9bda34d704e96f720838ec
Austidav59 <114312375+Austidav59@users.noreply.github.com>
2026-08-13 13:16:54 -0500

    fixed auth

--- apps/Frontend/src/main.tsx
+++ apps/Frontend/src/main.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
@@ -15,9 +14,7 @@ const root = document.getElementById("root");
 if (!root) throw new Error("Root element was not found");
 
 createRoot(root).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
 );
